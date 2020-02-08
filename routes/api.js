23429c2fa6f199836a441828cd71f2d43a3ac0c8
Paul Hendrickson <PaulRobHendrickson@gmail.com>
2020-02-08 17:34:18 -0600

    added html and api routes

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,45 @@
-const router = require("express").Router();
-const Workout = require("../models/workout.js");
+const db = require("../models");
 
-module.exports = router;
+module.exports = function(app) {
+  app.get("/api/workouts", function(req, res) {
+    db.Workout.find({})
+      .then(function(workouts) {
+        res.json(workouts);
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  });
+
+  app.post("/api/workouts", function(req, res) {
+    db.Workout.create({})
+      .then(function(workouts) {
+        res.json(workouts);
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  });
+
+  app.put("/api/workouts/:id", function(req, res) {
+    console.log(req.body);
+    db.Workout.where("_id", req.params.id)
+      .update({ $push: { exercises: req.body } })
+      .then(function(results) {
+        res.json(results);
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  });
+
+  app.get("/api/workouts/range", function(req, res) {
+    db.Workout.find({})
+      .then(function(workouts) {
+        res.json(workouts);
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  });
+};
